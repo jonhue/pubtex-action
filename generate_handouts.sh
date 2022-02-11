2e50d184d9b924814f5f51d71a04c5e99f9df088
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-02-11 09:50:10 +0100

    debug

--- generate_handouts.sh
+++ generate_handouts.sh
@@ -6,6 +6,8 @@ for slides in "$@"
 do
   echo "generating handout for $slides"
 
+  echo $slides
+
   handout=${slides:0:-4}_handout.tex
   cp $slides $handout
   sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" $handout
