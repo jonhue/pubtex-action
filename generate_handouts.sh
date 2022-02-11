c5afe6730a1019b9b96e2e854f2b19bc0f7cba47
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-02-11 08:54:37 +0100

    generate handouts

--- generate_handouts.sh
+++ generate_handouts.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-arg1=$1
-echo "arg1=$arg1"
+handouts=""
 
-# # Generate a handout
-# cp ./slides.tex ./handout.tex
-# sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" ./handout.tex
+for slides in "$@"
+do
+  echo "generating handout for $slides"
+
+  handout=${slides:0:-4}_handout.tex
+  cp $slides $handout
+  sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" $handout
+
+  handouts="$handouts $handout"
+done
+
+echo $handouts
+echo "handouts=$handouts" >> $GITHUB_ENV
