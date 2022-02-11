8a04e66698c811e3f212d6dabc6ddea4ecade78a
Jonas Hübotter <jonas.huebotter@gmail.com>
2022-02-11 10:04:31 +0100

    fixes

--- generate_handouts.sh
+++ generate_handouts.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# ${{ steps.generate-handouts.outputs.handouts }}
+
 handouts=""
 
 for slides in "$@"
 do
   echo "generating handout for $slides"
 
-  echo $(cat $slides)
-
   handout=${slides:0:-4}_handout.tex
   cp $slides $handout
   sed -i "1s/.*/\\\\documentclass\[handout\]\{beamer\}/" $handout
 
-  handouts="$handout" # !!!
+  handouts="$handouts\n$handout"
 done
 
 echo $handouts
